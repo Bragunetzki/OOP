c27d66c53e79e609ed0d3772b3333888fa854267
Bragunetzki <dragonbolty@gmail.com>
2021-10-01 13:34:23 +0700

    Added lab3.

--- Task112/src/test/java/ru/nsu/fit/oop/Task112/SubStringFinderTest.java
+++ Task112/src/test/java/ru/nsu/fit/oop/Task112/SubStringFinderTest.java
@@ -1,7 +1,21 @@
 package ru.nsu.fit.oop.Task112;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubStringFinderTest {
+
+    @Test
+    public void testFinderBasic() {
+        int[] trueResult = {7};
+        List<Integer> list = SubStringFinder.findSubString("input.txt", "пирог");
+
+        int[] result = list.stream().mapToInt(i->i).toArray();
+
+        assertArrayEquals(trueResult, result);
+    }
 
 }

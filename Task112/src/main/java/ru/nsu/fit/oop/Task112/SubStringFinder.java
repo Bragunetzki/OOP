5f46aee48319165bd0bb2f64b86915b1943f4007
Bragunetzki <dragonbolty@gmail.com>
2021-10-08 13:52:25 +0700

    Modified lab 2 to use Z-function-based algorithm.

--- Task112/src/main/java/ru/nsu/fit/oop/Task112/SubStringFinder.java
+++ Task112/src/main/java/ru/nsu/fit/oop/Task112/SubStringFinder.java
@@ -7,27 +7,47 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Contains functinos for finding all instances of a substring within a text file.
+ */
 public class SubStringFinder {
 
+    /**
+     * Finds all instances of inputString in file.
+     * @param filename - Name of text file within which to search.
+     * @param inputString - The substring that needs to be found.
+     * @return - Returns a list of indexes at which the substring is found.
+     */
     static List<Integer> findSubString(String filename, String inputString) {
         int ssLen = inputString.length();
         char[] subString = inputString.toCharArray();
+
         List<Integer> results = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
 
-            char[] buffer = new char[ssLen];
-            reader.mark(ssLen);
+            int bufferSize = 1000;
+            char[] buffer = new char[bufferSize+ssLen+1];
 
-            for (int index = 0; reader.read(buffer, 0, ssLen) != -1; index++) {
+            int[] zArr = new int[buffer.length];
+            buildZarr(subString, zArr, 0);
+            zArr[ssLen]=0;
 
-                if (Arrays.equals(buffer, subString)) {
-                    results.add(index);
+            System.arraycopy(subString, 0, buffer, 0, ssLen); //copy subString to beginning of buffer
+            buffer[ssLen]='0';
+            reader.mark(bufferSize);
+
+            while (reader.read(buffer, ssLen+1, bufferSize) != -1) {
+                buildZarr(buffer, zArr, ssLen+1);
+                for (int i = 1; i < zArr.length; i++) {
+                    if (zArr[i] == ssLen) {
+                        results.add(i-ssLen-1);
+                    }
                 }
 
                 reader.reset();
-                reader.read();
-                reader.mark(ssLen);
+                for (int i = 0; i < bufferSize+1; i++) reader.read();
+                reader.mark(bufferSize);
             }
         }
             catch (IOException e) {
@@ -35,5 +55,43 @@ public class SubStringFinder {
             }
 
         return results;
+    }
+
+    /**
+     * Builds an Z-function array for a string.
+     * @param str - The string that the array is built off of.
+     * @param zArr - The array in which to store data.
+     * @param startPos - The position with which to start modifying the array (useful when str starts with the pattern string).
+     */
+    static private void buildZarr(char[] str, int[] zArr, int startPos) {
+        int len = str.length;
+
+        int l = 0, r = 0;
+
+        for (int i = startPos; i < len; i++) {
+            if (i>r) {
+                l = r = i;
+
+                while (r < len && str[r-l]==str[r])
+                    r++;
+
+                zArr[i]=r-l;
+                r--;
+            }
+            else {
+                int k = i-l;
+
+                if (zArr[k]<r-i+1)
+                    zArr[i] = zArr[k];
+                else {
+                    l=i;
+                    while(r < len && str[r-l]==str[r])
+                        r++;
+
+                    zArr[i]=r-l;
+                    r--;
+                }
+            }
+        }
     }
 }
--- Task112/src/test/java/ru/nsu/fit/oop/Task112/SubStringFinderTest.java
+++ Task112/src/test/java/ru/nsu/fit/oop/Task112/SubStringFinderTest.java
@@ -1,21 +1,30 @@
 package ru.nsu.fit.oop.Task112;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubStringFinderTest {
-
     @Test
-    public void testFinderBasic() {
-        int[] trueResult = {0, 2, 4};
-        List<Integer> list = SubStringFinder.findSubString("input.txt", "aba");
+    public void testSubStringFinderBasic() {
+        int[] res = {0, 2, 4};
+        String file = "input.txt";
+        String subString = "aba";
+
+        List<Integer> list = SubStringFinder.findSubString(file, subString);
 
         int[] result = list.stream().mapToInt(i->i).toArray();
 
-        assertArrayEquals(trueResult, result);
+        assertArrayEquals(res, result);
     }
 
 }

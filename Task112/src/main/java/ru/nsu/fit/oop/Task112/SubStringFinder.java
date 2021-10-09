2899c4a92845eacdd0ff080b8ce95e6bd5ea220c
Bragunetzki <dragonbolty@gmail.com>
2021-10-09 15:14:30 +0700

    Modified lab 2 to allow for a custom reader to be used. Tests are now parameterized.

--- Task112/src/main/java/ru/nsu/fit/oop/Task112/SubStringFinder.java
+++ Task112/src/main/java/ru/nsu/fit/oop/Task112/SubStringFinder.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.oop.Task112;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,54 +14,70 @@ import java.util.List;
 public class SubStringFinder {
 
     /**
-     * Finds all instances of inputString in file.
-     * @param filename - Name of text file within which to search.
-     * @param inputString - The substring that needs to be found.
-     * @return - Returns a list of indexes at which the substring is found.
+     * Finds all occurences of a string within a file.
+     *
+     * @param filename    - the name of the file to search in.
+     * @param inputString - the string to search for.
+     * @return - returns a list of all indexes at which the substring can be found.
      */
-    static List<Integer> findSubString(String filename, String inputString) {
+    static List<Integer> findStringInFile(String filename, String inputString) {
+        try (FileReader reader = new FileReader(filename)) {
+            return findSubString(inputString, reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Finds a substring within a text.
+     *
+     * @param inputString - the string to search for.
+     * @param rdr         - the reader that provides the text.
+     * @return - returns a list of all indexes at which the substring can be found.
+     */
+    static List<Integer> findSubString(String inputString, Reader rdr) throws IOException {
+        if (inputString == null) throw new IllegalArgumentException("The input string is null!");
+
+        BufferedReader reader = new BufferedReader(rdr);
+
         int ssLen = inputString.length();
         char[] subString = inputString.toCharArray();
 
         List<Integer> results = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+        int bufferSize = 1000;
+        char[] buffer = new char[bufferSize + ssLen + 1];
 
-            int bufferSize = 1000;
-            char[] buffer = new char[bufferSize+ssLen+1];
+        int[] zArr = new int[buffer.length];
+        buildZarr(subString, zArr, 0);
+        zArr[ssLen] = 0;
 
-            int[] zArr = new int[buffer.length];
-            buildZarr(subString, zArr, 0);
-            zArr[ssLen]=0;
+        System.arraycopy(subString, 0, buffer, 0, ssLen); //copy subString to beginning of buffer
+        buffer[ssLen] = '0';
+        reader.mark(bufferSize);
 
-            System.arraycopy(subString, 0, buffer, 0, ssLen); //copy subString to beginning of buffer
-            buffer[ssLen]='0';
-            reader.mark(bufferSize);
-
-            while (reader.read(buffer, ssLen+1, bufferSize) != -1) {
-                buildZarr(buffer, zArr, ssLen+1);
-                for (int i = 1; i < zArr.length; i++) {
-                    if (zArr[i] == ssLen) {
-                        results.add(i-ssLen-1);
-                    }
+        while (reader.read(buffer, ssLen + 1, bufferSize) != -1) {
+            buildZarr(buffer, zArr, ssLen + 1);
+            for (int i = 1; i < zArr.length; i++) {
+                if (zArr[i] == ssLen) {
+                    results.add(i - ssLen - 1);
                 }
+            }
 
-                reader.reset();
-                for (int i = 0; i < bufferSize+1; i++) reader.read();
-                reader.mark(bufferSize);
-            }
+            reader.reset();
+            for (int i = 0; i < bufferSize + 1; i++) reader.read();
+            reader.mark(bufferSize);
         }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
 
         return results;
     }
 
     /**
      * Builds an Z-function array for a string.
-     * @param str - The string that the array is built off of.
-     * @param zArr - The array in which to store data.
+     *
+     * @param str      - The string that the array is built off of.
+     * @param zArr     - The array in which to store data.
      * @param startPos - The position with which to start modifying the array (useful when str starts with the pattern string).
      */
     static private void buildZarr(char[] str, int[] zArr, int startPos) {
@@ -69,26 +86,25 @@ public class SubStringFinder {
         int l = 0, r = 0;
 
         for (int i = startPos; i < len; i++) {
-            if (i>r) {
+            if (i > r) {
                 l = r = i;
 
-                while (r < len && str[r-l]==str[r])
+                while (r < len && str[r - l] == str[r])
                     r++;
 
-                zArr[i]=r-l;
+                zArr[i] = r - l;
                 r--;
-            }
-            else {
-                int k = i-l;
+            } else {
+                int k = i - l;
 
-                if (zArr[k]<r-i+1)
+                if (zArr[k] < r - i + 1)
                     zArr[i] = zArr[k];
                 else {
-                    l=i;
-                    while(r < len && str[r-l]==str[r])
+                    l = i;
+                    while (r < len && str[r - l] == str[r])
                         r++;
 
-                    zArr[i]=r-l;
+                    zArr[i] = r - l;
                     r--;
                 }
             }

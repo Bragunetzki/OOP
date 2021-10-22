49df17292725342df7c16d1d6896f90efe3a27b3
Bragunetzki <dragonbolty@gmail.com>
2021-10-23 01:37:38 +0700

    Lab 3.2 edit

--- Task132/src/main/java/ru/nsu/fit/oop/Gradebook.java
+++ Task132/src/main/java/ru/nsu/fit/oop/Gradebook.java
@@ -9,14 +9,26 @@ import java.util.Map;
 public class Gradebook {
     private Map<String, Integer> grades;
 
+    /**
+     * Initializes the gradebook.
+     */
     public Gradebook() {
         this.grades = new HashMap<String, Integer>();
     }
 
+    /**
+     * Adds a new subject and its grade.
+     *
+     * @param subject - title of subject to be added.
+     * @param grade - the grade itself.
+     */
     public void addGrade(String subject, Integer grade) {
         grades.put(subject, grade);
     }
 
+    /**
+     * @return - returns the average grade.
+     */
     public float averageGrade() {
         float avg = 0;
         for (Map.Entry<String, Integer> entry : grades.entrySet()) {
@@ -28,6 +40,9 @@ public class Gradebook {
         return avg;
     }
 
+    /**
+     * @return - returns true if an increased stipend is possible.
+     */
     public boolean increasedStipend() {
         for (Map.Entry<String, Integer> entry : grades.entrySet()) {
             if(entry.getValue() != 5) {
@@ -38,6 +53,9 @@ public class Gradebook {
         return true;
     }
 
+    /**
+     * @return - returns true if a red diploma is possible.
+     */
     public boolean redDiploma() {
         int count5 = 0;
 
@@ -46,7 +64,14 @@ public class Gradebook {
                 return false;
             }
 
-
+            if(entry.getValue() == 5) {
+                count5++;
+            }
         }
+
+       float ratioOf5 = (float) count5/grades.size();
+
+        if (ratioOf5 >= 0.75) return true;
+        else return false;
     }
 }

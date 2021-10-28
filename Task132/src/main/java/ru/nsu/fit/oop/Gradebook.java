1c50ec9c48ae894bb975ff4af4a4cccc1c36bc6d
Bragunetzki <dragonbolty@gmail.com>
2021-10-28 12:11:51 +0700

    Lab 3.2 edited to present an array of semesters.

--- Task132/src/main/java/ru/nsu/fit/oop/Gradebook.java
+++ Task132/src/main/java/ru/nsu/fit/oop/Gradebook.java
@@ -7,23 +7,28 @@ import java.util.Map;
  * Contains a student's grades.
  */
 public class Gradebook {
-    private Map<String, Integer> grades;
+    private HashMap<String, Integer>[] grades;
+    private int gradecount;
 
     /**
      * Initializes the gradebook.
      */
     public Gradebook() {
-        this.grades = new HashMap<String, Integer>();
+        grades = (HashMap<String, Integer>[]) new HashMap[8];
+        for (int i = 0; i < 8; i++) grades[i] = new HashMap<String, Integer>();
+        gradecount = 0;
     }
 
     /**
      * Adds a new subject and its grade.
      *
      * @param subject - title of subject to be added.
-     * @param grade - the grade itself.
+     * @param sem     - number of semester to add grade to.
+     * @param grade   - the grade itself.
      */
-    public void addGrade(String subject, Integer grade) {
-        grades.put(subject, grade);
+    public void addGrade(String subject, int sem, Integer grade) {
+        grades[sem - 1].put(subject, grade);
+        gradecount++;
     }
 
     /**
@@ -31,21 +36,27 @@ public class Gradebook {
      */
     public float averageGrade() {
         float avg = 0;
-        for (Map.Entry<String, Integer> entry : grades.entrySet()) {
-            avg += entry.getValue();
+
+        for (int i = 0; i < 8; i++) {
+            for (Map.Entry<String, Integer> entry : grades[i].entrySet()) {
+                avg += entry.getValue();
+            }
         }
 
-        avg /= grades.size();
+        avg /= gradecount;
 
         return avg;
     }
 
     /**
+     * @param sem - current semester.
      * @return - returns true if an increased stipend is possible.
      */
-    public boolean increasedStipend() {
-        for (Map.Entry<String, Integer> entry : grades.entrySet()) {
-            if(entry.getValue() != 5) {
+    public boolean increasedStipend(int sem) {
+        if (sem > 8 || sem < 1) throw new IllegalArgumentException("Semester value has to be between 1 and 8!");
+
+        for (Map.Entry<String, Integer> entry : grades[sem - 1].entrySet()) {
+            if (entry.getValue() != 5) {
                 return false;
             }
         }
@@ -57,19 +68,23 @@ public class Gradebook {
      * @return - returns true if a red diploma is possible.
      */
     public boolean redDiploma() {
-        int count5 = 0;
+        Map<String, Integer> diploma = new HashMap<String, Integer>();
 
-        for (Map.Entry<String, Integer> entry : grades.entrySet()) {
-            if(entry.getValue() == 3) {
-                return false;
-            }
+        for (int i = 0; i < 8; i++) {
+            for (Map.Entry<String, Integer> entry : grades[i].entrySet()) {
+                if (entry.getValue() == 3) {
+                    return false;
+                }
 
-            if(entry.getValue() == 5) {
-                count5++;
+                if (entry.getValue() == 5) {
+                    if (!diploma.containsKey(entry.getKey())) {
+                        diploma.put(entry.getKey(), entry.getValue());
+                    }
+                }
             }
         }
 
-       float ratioOf5 = (float) count5/grades.size();
+        float ratioOf5 = (float) diploma.size() / gradecount;
 
         if (ratioOf5 >= 0.75) return true;
         else return false;
--- Task132/src/test/java/ru/nsu/fit/oop/GradebookTest.java
+++ Task132/src/test/java/ru/nsu/fit/oop/GradebookTest.java
@@ -1,24 +1,35 @@
 package ru.nsu.fit.oop;
 
+import com.sun.jdi.connect.Connector;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GradebookTest {
-    public Gradebook testBook() {
+
+    private Gradebook testBook() {
         Gradebook book = new Gradebook();
-        book.addGrade("Introduction to algebra and math analysis - sem1", 3);
-        book.addGrade("Introduction to discrete maths and mathematical logic - sem1", 4);
-        book.addGrade("History", 4);
-        book.addGrade("Declarative programming - sem1", 4);
-        book.addGrade("Basics of speech culture", 4);
-        book.addGrade("Imperative programming - sem1", 5);
-        book.addGrade("Introduction to algebra and math analysis - sem2", 4);
-        book.addGrade("Imperative programming - sem2", 5);
-        book.addGrade("Digital platforms", 5);
-        book.addGrade("Introduction to discrete maths and mathematical logic - sem2", 5);
-        book.addGrade("English - sem2", 4);
-        book.addGrade("Declarative programming - sem2", 4);
+
+        book.addGrade("Introduction to algebra and math analysis", 1, 3);
+        book.addGrade("Introduction to discrete maths and mathematical logic", 1, 4);
+        book.addGrade("History", 1, 4);
+        book.addGrade("Declarative programming", 1, 4);
+        book.addGrade("Basics of speech culture", 1, 4);
+        book.addGrade("Imperative programming", 1, 5);
+        book.addGrade("Introduction to algebra and math analysis", 2, 4);
+        book.addGrade("Imperative programming", 2, 5);
+        book.addGrade("Digital platforms", 1, 5);
+        book.addGrade("Introduction to discrete maths and mathematical logic", 2, 5);
+        book.addGrade("English", 2, 4);
+        book.addGrade("Declarative programming", 2, 4);
 
         return book;
     }
@@ -34,20 +45,22 @@ class GradebookTest {
     public void testGradebookIncreasedStipend() {
         Gradebook badBook = testBook();
         Gradebook goodBook = new Gradebook();
-        goodBook.addGrade("SomeSubject", 5);
+        goodBook.addGrade("SomeSubject", 1, 5);
+        goodBook.addGrade("SomeOtherSubject", 2, 4);
 
-        assertTrue(goodBook.increasedStipend());
-        assertFalse(badBook.increasedStipend());
+        assertTrue(goodBook.increasedStipend(1));
+        assertFalse(badBook.increasedStipend(1));
+        assertFalse(goodBook.increasedStipend(2));
     }
 
     @Test
     public void testGradebookRedDiploma() {
         Gradebook badBook = testBook();
         Gradebook goodBook = new Gradebook();
-        goodBook.addGrade("Somesubject", 5);
-        goodBook.addGrade("Somesubject1", 5);
-        goodBook.addGrade("Somesubject2", 5);
-        goodBook.addGrade("Somesubject3", 4);
+        goodBook.addGrade("Somesubject",1, 5);
+        goodBook.addGrade("Somesubject1",1, 5);
+        goodBook.addGrade("Somesubject2", 1,5);
+        goodBook.addGrade("Somesubject3",1, 4);
 
         assertTrue(goodBook.redDiploma());
         assertFalse(badBook.redDiploma());

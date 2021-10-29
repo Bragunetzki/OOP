16a071ee6dbc51797ac0abc2c7e119493591523a
Bragunetzki <dragonbolty@gmail.com>
2021-10-29 14:05:01 +0700

    Rebased lab 4.1

---
+++ Task141/src/main/java/ru/nsu/fit/oop/Main.java
@@ -0,0 +1,24 @@
+package ru.nsu.fit.oop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter an expression in prefix notation (and use spaces!):");
+        String input = reader.readLine();
+
+        while (!input.equals("0")) {
+            System.out.printf("Result: %f\n", Calculator.parse(input));
+            System.out.print("Enter an expression in prefix notation (and use spaces!):");
+            input = reader.readLine();
+        }
+
+        reader.close();
+    }
+
+}

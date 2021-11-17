860a4b0428075714e52a7a5bb74817231d21c539
Bragunetzki <dragonbolty@gmail.com>
2021-11-17 15:58:34 +0700

    Modified lab 4.1. to generate operations as classes that implement the Operation interface.

--- Task141/src/main/java/ru/nsu/fit/oop/Calculator.java
+++ Task141/src/main/java/ru/nsu/fit/oop/Calculator.java
@@ -2,123 +2,221 @@ package ru.nsu.fit.oop;
 
 import java.io.InputStream;
 import java.util.*;
-import java.util.regex.Pattern;
+import java.lang.reflect.*;
 
 /**
  * Implements a simple calculator that parses a string in prefix notation.
  */
-public class Calculator {
+abstract class Calculator {
     /**
-     * Parses a valid prefix notation string and returns the result.
-     *
-     * @param input - the string that needs to be parsed.
-     * @return - returns a result of calculations as double.
+     * The interface that all operations implement.
+     */
+    interface Operation {
+        /**
+         * Contains the actual calculation of an operation. Each operation defines what this method does.
+         * @param operands array of operands that are used in operation.
+         * @return returns the result of operation as double.
+         */
+        double result(double[] operands);
+
+        /**
+         * Gives the arity of each operation
+         * @return returns the arity of the operation.
+         */
+        int arity();
+
+        /**
+         * Parses the operands needed for the operation.
+         * If another operation is encountered while parsing, recursively calls the other operation first.
+         * The result of any operation called from here becomes an operand.
+         * @param parser the scanner that is used to get values.
+         * @return returns the result of operation as double.
+         */
+        default double calculate(Scanner parser) {
+            double[] operands = new double[arity()];
+
+            for (int i = 0; i < arity(); i++) {
+                if (parser.hasNextDouble()) {
+                    operands[i] = parser.nextDouble();
+                } else {
+                    Operation oper = callOperation(parser.next());
+                    operands[i] = oper.calculate(parser);
+                }
+            }
+
+            return result(operands);
+        }
+    }
+
+    /**
+     * Calculates the sum of two numbers.
+     */
+    static class Sum implements Operation {
+        public double result(double[] operands) {
+            return operands[0] + operands[1];
+        }
+
+        public int arity() {
+            return 2;
+        }
+    }
+
+    /**
+     * Substracts one number from another.
+     */
+    static class Diff implements Operation {
+        public double result(double[] operands) {
+            return operands[0] - operands[1];
+        }
+
+        public int arity() {
+            return 2;
+        }
+    }
+
+    /**
+     * Multiplies one number by another.
+     */
+    static class Mult implements Operation {
+        public double result(double[] operands) {
+            return operands[0] * operands[1];
+        }
+
+        public int arity() {
+            return 2;
+        }
+    }
+
+    /**
+     * Divides one number by another.
+     */
+    static class Div implements Operation {
+        public double result(double[] operands) {
+            return operands[0] / operands[1];
+        }
+
+        public int arity() {
+            return 2;
+        }
+    }
+
+    /**
+     * Calculates the value of one number to the power of another number.
+     */
+    static class Pow implements Operation {
+        public double result(double[] operands) {
+            return Math.pow(operands[0], operands[1]);
+        }
+
+        public int arity() {
+            return 2;
+        }
+    }
+
+    /**
+     * Calculates the log of a number.
+     */
+    static class Log implements Operation {
+        public double result(double[] operands) {
+            return Math.log(operands[0]);
+        }
+
+        public int arity() {
+            return 1;
+        }
+    }
+
+    /**
+     * Calculates the sine of a number.
+     */
+    static class Sin implements Operation {
+        public double result(double[] operands) {
+            return Math.sin(operands[0]);
+        }
+
+        public int arity() {
+            return 1;
+        }
+    }
+
+    /**
+     * Calculates the cosine of a number.
+     */
+    static class Cos implements Operation {
+        public double result(double[] operands) {
+            return Math.cos(operands[0]);
+        }
+
+        public int arity() {
+            return 1;
+        }
+    }
+
+    /**
+     * Calculates the square root of a number.
+     */
+    static class Sqrt implements Operation {
+        public double result(double[] operands) {
+            return Math.sqrt(operands[0]);
+        }
+
+        public int arity() {
+            return 1;
+        }
+    }
+
+    /**
+     * Selects an operation depending on the provided token.
+     * @param operation the token that determines the operation.
+     * @return returns an instance of the selected operation.
+     */
+    static Operation callOperation(String operation) {
+       switch (operation) {
+            case ("+") -> {
+                return new Sum();
+            }
+            case ("-") -> {
+                return new Diff();
+            }
+            case ("*") -> {
+                return new Mult();
+            }
+            case ("/") -> {
+                return new Div();
+            }
+            case ("log") -> {
+                return new Log();
+            }
+            case ("pow") -> {
+                return new Pow();
+            }
+            case ("sqrt") -> {
+                return new Sqrt();
+            }
+            case ("sin") -> {
+                return new Sin();
+            }
+            case ("cos") -> {
+                return new Cos();
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + operation);
+        }
+    }
+
+    /**
+     * Calculates the result of a valid expression in polish notation.
+     * @param input - the string that contains the expression.
+     * @return - returns the result of the calculation as a double.
      */
     public static double calculate(String input) {
         Scanner parser = new Scanner(input);
 
-        Stack<Character> operators = new Stack<>();
-        Stack<Integer> arities = new Stack<>();
-
-        Stack<Double> operands = new Stack<>();
-        Stack<Integer> operandGroups = new Stack<>();
-
-        int lastGroup = 0;
-
-        while (parser.hasNext()) {
-
-            if (parser.hasNextDouble()) {
-                operands.push(parser.nextDouble());
-                lastGroup++;
-            } else if (parser.hasNext(".")) {
-                Character token = parser.findInLine(". ").charAt(0);
-                operators.push(token);
-                arities.push(2);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            } else if (parser.hasNext("log")) {
-                parser.next("log");
-                operators.push('l');
-                arities.push(1);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            } else if (parser.hasNext("pow")) {
-                parser.next("pow");
-                operators.push('p');
-                arities.push(2);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            } else if (parser.hasNext("sqrt")) {
-                parser.next("sqrt");
-                operators.push('r');
-                arities.push(1);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            } else if (parser.hasNext("sin")) {
-                parser.next("sin");
-                operators.push('s');
-                arities.push(1);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            } else if (parser.hasNext("cos")) {
-                parser.next("cos");
-                operators.push('c');
-                arities.push(1);
-                operandGroups.push(lastGroup);
-                lastGroup = 0;
-            }
-
-            // System.out.printf("top arity is %d and top groupsize is %d\n", arities.peek(), lastGroup);
-            while (arities.peek() == lastGroup) {
-                double operand1 = operands.pop();
-                double operand2;
-                double res = 0;
-
-                switch (operators.pop()) {
-                    case ('+') -> {
-                        operand2 = operands.pop();
-                        res = operand2 + operand1;
-                    }
-                    case ('-') -> {
-                        operand2 = operands.pop();
-                        res = operand2 - operand1;
-                    }
-                    case ('*') -> {
-                        operand2 = operands.pop();
-                        res = operand2 * operand1;
-                    }
-                    case ('/') -> {
-                        operand2 = operands.pop();
-                        res = operand2 / operand1;
-                    }
-                    case ('l') -> {
-                        res = Math.log(operand1);
-                    }
-                    case ('p') -> {
-                        operand2 = operands.pop();
-                        res = Math.pow(operand2, operand1);
-                    }
-                    case ('r') -> {
-                        res = Math.sqrt(operand1);
-                    }
-                    case ('s') -> {
-                        res = Math.sin(operand1);
-                    }
-                    case ('c') -> {
-                        res = Math.cos(operand1);
-                    }
-                }
-
-                operands.push(res);
-                lastGroup = operandGroups.pop() + 1;
-                arities.pop();
-
-                if (arities.empty()) break;
-            }
+        if (parser.hasNextDouble()) {
+            return parser.nextDouble();
         }
 
-        parser.close();
-
-        return operands.pop();
+        String operation = parser.next();
+        Operation oper = callOperation(operation);
+        return oper.calculate(parser);
     }
 }

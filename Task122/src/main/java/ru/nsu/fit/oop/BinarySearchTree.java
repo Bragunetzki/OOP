5e091dbe4fbcbf8b05cbdf67e4d4da7ca1c318e4
Bragunetzki <dragonbolty@gmail.com>
2021-12-28 00:36:21 +0700

    Added tests and Stream API with custom Spliterator support to lab 122

--- Task122/src/main/java/ru/nsu/fit/oop/BinarySearchTree.java
+++ Task122/src/main/java/ru/nsu/fit/oop/BinarySearchTree.java
@@ -1,7 +1,12 @@
 package ru.nsu.fit.oop;
 
 import java.util.*;
+import java.util.function.Consumer;
 
+/**
+ * Represents a binary search tree collection.
+ * @param <E> - the type of element stored in the tree.
+ */
 public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> {
     private E val;
     private BinarySearchTree<E> parent, left, right;
@@ -9,6 +14,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
     private int lsize;
     private int rsize;
 
+    /**
+     * Constructs a tree with a given parent and data.
+     * @param data - data that is stored at the root of the new tree.
+     * @param parent - the new tree's parent tree.
+     */
     public BinarySearchTree(E data, BinarySearchTree<E> parent) {
         this.parent = parent;
         val = data;
@@ -16,56 +26,107 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         rsize = 0;
     }
 
+    /**
+     * Constructs a new tree without a parent.
+     * @param data - data that is stored at the root of the new tree.
+     */
     public BinarySearchTree(E data) {
         this(data, null);
     }
 
+    /**
+     * Constructs an empty tree.
+     */
     public BinarySearchTree() {
     }
 
+    /**
+     * Constructs a tree that copies all elements of another tree.
+     * @param tree - the tree that should be copied.
+     */
     public BinarySearchTree(BinarySearchTree<E> tree) {
         this.addAll(tree);
     }
 
+    /**
+     * Returns the value at the root of the tree.
+     * @return - value of type E.
+     */
     public E value() {
         return val;
     }
 
+    /**
+     * Returns the parent of the tree.
+     * @return - parent of tree.
+     */
     public BinarySearchTree<E> getParent() {
         return parent;
     }
 
+    /**
+     * Returns the left child of the tree.
+     * @return - left child of tree.
+     */
     public BinarySearchTree<E> getLeft() {
         return left;
     }
 
+    /**
+     * Returns the right child of the tree.
+     * @return - right child of tree.
+     */
     public BinarySearchTree<E> getRight() {
         return right;
     }
 
+    /**
+     * Returns number of left children of tree.
+     * @return - size of left subtree.
+     */
     public int getLsize() {
         return lsize;
     }
 
+    /**
+     * Returns number of right children of tree.
+     * @return  - size of right subtree.
+     */
     public int getRsize() {
         return rsize;
     }
 
+    /**
+     * Sets the value at the root of the tree to the given value.
+     * @param e - the value that should be set.
+     */
     public void set(E e) {
         if (e == null) throw new NullPointerException();
         val = e;
     }
 
+    /**
+     * Returns the size of the tree.
+     * @return - size of tree.
+     */
     @Override
     public int size() {
         return lsize + rsize + (val == null ? 0 : 1);
     }
 
+    /**
+     * @return - returns true if tree is empty and false if tree has any elements.
+     */
     @Override
     public boolean isEmpty() {
         return (size() == 0);
     }
 
+    /**
+     * Checks if the tree contains a given object.
+     * @param o - the object that should be searched for.
+     * @return - returns true if the tree contains the object and false otherwise.
+     */
     @Override
     public boolean contains(Object o) {
         for (E e : this) {
@@ -74,21 +135,26 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return false;
     }
 
+    /**
+     * Returns an iterator that can iterate through the tree.
+     * @return - returns the iterator.
+     */
     @Override
     public Iterator<E> iterator() {
         return new TreeIterator(this);
     }
 
+    /**
+     * The iterator class that works with this tree.
+     */
     private class TreeIterator implements Iterator<E> {
         private E next;
         private BinarySearchTree<E> nextTree;
+        private BinarySearchTree<E> last;
 
         public TreeIterator(BinarySearchTree<E> parentTree) {
-            nextTree = parentTree;
-
-            while (nextTree.getLeft() != null) {
-                nextTree = nextTree.getLeft();
-            }
+            nextTree = minNode();
+            last = maxNode();
         }
 
         @Override
@@ -100,28 +166,130 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         public E next() {
             E res = nextTree.value();
 
-            if (nextTree.right != null) {
-                nextTree = nextTree.right;
-                while (nextTree.left != null) {
-                    nextTree = nextTree.left;
-                }
+            if (nextTree == last) {
+                nextTree = null;
                 return res;
             }
 
-            while (true) {
-                if (nextTree.parent == null) {
-                    nextTree = null;
-                    return res;
-                }
-                if (nextTree.parent.left.equals(nextTree)) {
-                    nextTree = nextTree.parent;
-                    return res;
-                }
+            if (nextTree.right != null) {
+                nextTree = nextTree.right.minNode();
+            } else if (nextTree.parent != null) {
+                BinarySearchTree<E> prev = nextTree;
                 nextTree = nextTree.parent;
+                while (prev == nextTree.right) {
+                    prev = nextTree;
+                    nextTree = nextTree.parent;
+                }
             }
+            return res;
         }
     }
 
+    /**
+     * Finds the minimum child of given tree.
+     * @return - returns the minimum leaf of tree.
+     */
+    private BinarySearchTree<E> minNode() {
+        BinarySearchTree<E> res = this;
+        while (res.left != null) {
+            res = res.left;
+        }
+
+        return res;
+    }
+
+    /**
+     * Finds the maximum child of given tree.
+     * @return - returns the maximum leaf of tree.
+     */
+    private BinarySearchTree<E> maxNode() {
+        BinarySearchTree<E> res = this;
+        while (res.right != null) {
+            res = res.right;
+        }
+
+        return res;
+    }
+
+    /**
+     * Returns a spliterator that can iterate through the tree.
+     * @return - returns the spliterator.
+     */
+    @Override
+    public Spliterator<E> spliterator() {
+        return new TreeSpliterator(this);
+    }
+
+    /**
+     * The spliterator class that works with this tree.
+     */
+    private class TreeSpliterator implements Spliterator<E> {
+        private E curr_val;
+        private BinarySearchTree<E> nextTree;
+        private BinarySearchTree<E> last;
+
+        public TreeSpliterator(BinarySearchTree<E> parentTree) {
+            nextTree = minNode();
+            last = maxNode();
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super E> action) {
+            if (nextTree != null) {
+                E res = nextTree.val;
+
+                if (nextTree == last) {
+                    action.accept(res);
+                    nextTree = null;
+                    return true;
+                }
+
+                if (nextTree.right != null) {
+                    nextTree = nextTree.right.minNode();
+                } else if (nextTree.parent != null) {
+                    BinarySearchTree<E> prev = nextTree;
+                    nextTree = nextTree.parent;
+                    while (prev == nextTree.right) {
+                        prev = nextTree;
+                        nextTree = nextTree.parent;
+                    }
+                }
+
+                action.accept(res);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public Spliterator<E> trySplit() {
+            if (estimateSize() <= 1) return null;
+
+            nextTree = nextTree.left;
+            return new TreeSpliterator(nextTree.right);
+        }
+
+        @Override
+        public long estimateSize() {
+            return nextTree.size();
+        }
+
+        @Override
+        public int characteristics() {
+            return ORDERED | DISTINCT | SORTED | SIZED | SUBSIZED;
+        }
+
+        @Override
+        public Comparator<? super E> getComparator() {
+            return null;
+        }
+    }
+
+    /**
+     * Converts tree to array.
+     * @return - returns an array containing the ordered elements of the tree.
+     */
     @Override
     public Object[] toArray() {
         Object[] arr = new Object[size()];
@@ -135,6 +303,12 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return arr;
     }
 
+    /**
+     * Converts tree to array and attempts to fill the given array with the data.
+     * @param a 0 the array that should be filled.
+     * @param <T> - the type of element contained in array.
+     * @return - returns an array containing the ordered elements of the tree.
+     */
     @Override
     @SuppressWarnings("unchecked")
     public <T> T[] toArray(T[] a) {
@@ -154,6 +328,13 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
     }
 
 
+    /**
+     * Adds an element to the tree. The tree is not self-balancing,
+     * so one should take care when adding an ordered list of elements.
+     * Duplicate elements do not alter the tree.
+     * @param e - the element that should be added to the tree.
+     * @return - returns true if the tree changed after the element was added, returns false otherwise.
+     */
     @Override
     public boolean add(E e) {
         //A null element does not alter the tree.
@@ -185,8 +366,18 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return true;
     }
 
+    /**
+     * Reduces the size of all predecessor trees by 1.
+     * @param tree - the tree from which to start.
+     */
     private void reduceParentSize(BinarySearchTree<E> tree) {
         while (tree.parent != null) {
+            if (tree.parent.left == null) {
+                tree.parent.rsize--;
+                reduceParentSize(tree.parent);
+                return;
+            }
+
             if (tree.parent.left.equals(tree)) {
                 tree.parent.lsize--;
             } else {
@@ -197,6 +388,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         }
     }
 
+    /**
+     * Removes an object from the tree.
+     * @param o - the object that should be removed.
+     * @return - returns true if the tree was changed after the element was removed, returns false otherwise.
+     */
     @Override
     @SuppressWarnings("unchecked")
     public boolean remove(Object o) {
@@ -207,13 +403,27 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return removeNode(removedTree);
     }
 
+    /**
+     * Removes a specific subtree from the tree.
+     * @param removedTree - the subtree that should be removed.
+     * @return - returns true if the tree was changed after the subtree was removed, returns false otherwise
+     */
     private boolean removeNode(BinarySearchTree<E> removedTree) {
         if (removedTree == null) return false;
         BinarySearchTree<E> parent = removedTree.parent;
 
         //leaf needs to be removed;
         if (removedTree.rsize == 0 && removedTree.lsize == 0) {
-            if (parent == null) removedTree.val = null;
+            if (parent == null) {
+                removedTree.val = null;
+                return true;
+            }
+
+            if (parent.left == null) {
+                parent.right = null;
+                reduceParentSize(removedTree);
+                return true;
+            }
 
             if (parent.left.equals(removedTree)) {
                 parent.left = null;
@@ -254,6 +464,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
     }
 
 
+    /**
+     * Finds the next in-order successor of an element.
+     * @param e - the element that should be compared to.
+     * @return - returns the subtree containing the successor.
+     */
     public BinarySearchTree<E> findSuccessor(E e) {
         if (e.compareTo(val) < 0) {
             if (lsize == 0) return this;
@@ -266,6 +481,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         }
     }
 
+    /**
+     * Finds a subtree containing the specified element.
+     * @param e - the element which the subtree should contain.
+     * @return - returns the subtree containing the element.
+     */
     private BinarySearchTree<E> findSubTree(E e) {
 
         if (val.compareTo(e) == 0) return this;
@@ -279,6 +499,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         }
     }
 
+    /**
+     * Checks if the tree contains all elements of given collection.
+     * @param c - the collection that should be compared to.
+     * @return - returns true if the tree contains each element of collection, and returns false otherwise.
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         for (Object o : c) {
@@ -289,6 +514,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return true;
     }
 
+    /**
+     * Adds all elements of given collection to the tree.
+     * @param c - the collection from which to pool elements.
+     * @return - returns true if the tree was modified after the addition, returns false otherwise.
+     */
     @Override
     public boolean addAll(Collection<? extends E> c) {
         boolean flag = false;
@@ -302,6 +532,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return flag;
     }
 
+    /**
+     * Removes all elements contained in the collection from the tree.
+     * @param c - the collection from which to take elements.
+     * @return - returns true if the tree was modified after the addition, returns false otherwise.
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         boolean flag = true;
@@ -312,10 +547,15 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return flag;
     }
 
+    /**
+     * Removes all elements from the tree, except for those contained in the given collection.
+     * @param c - the collection which to compare with.
+     * @return - returns true if the tree was modified after the operation, returns false otherwise.
+     */
     @Override
     public boolean retainAll(Collection<?> c) {
         ArrayList<E> removeList = new ArrayList<>();
-        for (E e: this) {
+        for (E e : this) {
             if (!c.contains(e)) removeList.add(e);
         }
 
@@ -326,6 +566,9 @@ public class BinarySearchTree<E extends Comparable<E>> implements Collection<E> 
         return (!removeList.isEmpty());
     }
 
+    /**
+     * Removes all elements from the tree.
+     */
     @Override
     public void clear() {
         ArrayList<E> clearList = new ArrayList<>(this);
--- Task122/src/test/java/ru/nsu/fit/oop/TreeTest.java
+++ Task122/src/test/java/ru/nsu/fit/oop/TreeTest.java
@@ -11,7 +11,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
     @Test
-    public void TreeTestSize() {
+    public void treeTestStreamAPI() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.addAll(Arrays.asList(7, 3, 5, 4, 9, 13, 6));
+
+        Integer sumOdd = tree.stream().filter(o -> o % 2 != 0).reduce(Integer::sum).orElse(0);
+        Integer sumEven = tree.stream().filter(o -> o % 2 == 0).reduce(Integer::sum).orElse(0);
+
+        assertEquals(sumOdd,37);
+        assertEquals(sumEven, 10);
+    }
+
+    @Test
+    public void treeTestStreamAPIStrings() {
+        BinarySearchTree<String> tree = new BinarySearchTree<>();
+        tree.addAll(Arrays.asList("abababa", "ab", "bab", "ba", "123", "hello, there"));
+        Object[] expected = new String[] {"ab", "abababa", "bab"};
+
+        Stream<String> stream = tree.stream().filter(str -> str.contains("ab"));
+
+        Object[] arr = stream.toArray();
+
+        assertArrayEquals(arr, expected);
+    }
+
+    @Test
+    public void treeTestSize() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>(5);
         tree.add(5);
         tree.add(4);
@@ -20,19 +45,17 @@ class TreeTest {
         tree.add(2);
         tree.add(-8);
 
-        for (Integer i : tree) System.out.println(i);
-
         assertEquals(5, tree.size());
     }
 
     @Test
-    public void TreeTestEmpty() {
+    public void treeTestEmpty() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         assertTrue(tree.isEmpty());
     }
 
     @Test
-    public void TreeTestAddNull() {
+    public void treeTestAddNull() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         assertFalse(tree.add(null));
         tree.add(5);
@@ -40,7 +63,7 @@ class TreeTest {
     }
 
     @Test
-    public  void TreeTestContains() {
+    public void treeTestContains() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         List<Integer> list = new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1));
         tree.addAll(Arrays.asList(5, 4, 3, 2, 1));
@@ -49,6 +72,58 @@ class TreeTest {
         assertFalse(tree.contains(10));
     }
 
+    @Test
+    public void treeTestToArray() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        Integer[] arr1 = new Integer[]{1, 2, 3, 4, 5};
+        tree.addAll(Arrays.asList(arr1));
 
+        Integer[] arr2 = tree.toArray(new Integer[tree.size()]);
 
+        assertArrayEquals(arr1, arr2);
+    }
+
+    @Test
+    public void treeTestClear() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        assertEquals(5, tree.size());
+        tree.clear();
+        assertEquals(0, tree.size());
+    }
+
+    @Test
+    public void treeTestRemove() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        tree.remove(5);
+
+        Integer[] arr2 = tree.toArray(new Integer[tree.size()]);
+
+        assertArrayEquals(arr2, new Integer[] {1, 2, 3, 4});
+    }
+
+    @Test
+    public void treeTestRemoveAll() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        Integer[] arr1 = new Integer[]{1, 2, 3, 4, 5};
+        tree.addAll(Arrays.asList(arr1));
+
+        tree.removeAll(Arrays.asList(1, 2, 3));
+        Integer[] arr2 = tree.toArray(new Integer[tree.size()]);
+
+        assertArrayEquals(arr2, new Integer[] {4, 5});
+    }
+
+    @Test
+    public void treeTestRetainAll() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        Integer[] arr1 = new Integer[]{1, 2, 3, 4, 5};
+        tree.addAll(Arrays.asList(arr1));
+
+        tree.retainAll(Arrays.asList(1, 2, 3));
+        Integer[] arr2 = tree.toArray(new Integer[tree.size()]);
+
+        assertArrayEquals(arr2, new Integer[] {1, 2, 3});
+    }
 }
